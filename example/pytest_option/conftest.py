12f13aa5e6d5c3c5d2dad498fb1fc1d4f45fd6b1
g_xin <g_xin@outlook.com>
2023-03-26 10:24:14 +0800

    Fix

---
+++ example/pytest_option/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from web_test.alternative.pytest.project.settings import Option
+
+
+class Config:
+
+    def __init__(self, request):
+        self.request = request
+
+    @Option.default(6.0)
+    def timeout(self):
+        pass
+
+    @Option.default("gu xin")
+    def author(self):
+        pass
+
+
+def pytest_addoption(parser):
+    Option.register_all(from_cls=Config, in_parser=parser)
+
+
+@pytest.fixture
+def config(request):
+    return Config(request)
+
+
+@pytest.fixture(scope='function', autouse=True)
+def timeout_management(config):
+    timeout = config.timeout
+    yield timeout
+
+
+@pytest.fixture(scope='function', autouse=True)
+def author_management(config):
+    author = config.author
+    yield author
---
+++ example/pytest_option/pytest_option_test.py
@@ -0,0 +1,6 @@
+def test_timeout(timeout_management):
+    assert timeout_management == 6.0
+
+
+def test_author(author_management):
+    assert author_management == 'gu xin'
