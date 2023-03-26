dabfe2fd35d9f9c38719ec683972c88ee07c0899
g_xin <g_xin@outlook.com>
2023-03-26 15:00:07 +0800

    Fix

--- tests/test_serach.py
+++
@@ -1,9 +0,0 @@
-from web_test import app
-
-
-def test_google():
-    app.google.open()
-
-    app.google \
-        .search('yashaka selene python') \
-        .should_have_results_amount_at_least(12)
