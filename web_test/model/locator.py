1a6e4123e137a206c98190b22e3295e20c8fcd79
g_xin <g_xin@outlook.com>
2023-03-26 21:10:05 +0800

    Fix

---
+++ web_test/model/locator.py
@@ -0,0 +1,42 @@
+from typing import Callable
+
+from selene import by
+from selene.support.shared.jquery_style import s
+
+
+class LocatorConfig:
+    locate_rule: Callable = None
+
+    def get_element(self, tag):
+        return self.Label(self.locate_rule, tag)
+
+    @property
+    def input_box(self):
+        return self.get_element('input')
+
+    @property
+    def select(self):
+        return self.get_element('select')
+
+    @property
+    def textarea(self):
+        return self.get_element('textarea')
+
+    @property
+    def text(self):
+        return self.get_element('span')
+
+    @property
+    def button(self):
+        return self.get_element('button')
+
+    class Label:
+
+        def __init__(self, locate_function: Callable[[str], str], tag: str):
+            self.locate_function = locate_function
+            self.tag = tag
+
+        def behind_label(self, label_value):
+            by_, value = by.text(label_value)
+            xpath = f'{self.locate_function(value)}//{self.tag}'
+            return s(by.xpath(xpath))
