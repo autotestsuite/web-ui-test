072c07ec3056594e15a8baf2ce704786f4bfb000
g_xin <g_xin@outlook.com>
2023-03-26 11:00:35 +0800

    Fix

---
+++ example/pytest_settings/settings.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from web_test.alternative.pytest.settings import sourced
+
+
+class Settings(sourced.Settings):
+
+    @sourced.default(6.0)
+    def timeout(self): pass
+
+    @sourced.default('gu xin')
+    def author(self): pass
+
+
+config = Settings(
+    lambda key, _: json.load(open('settings.json')).get(key),
+    os.getenv,
+)
+
+
+if __name__ == '__main__':
+    print(config.timeout)
+    print(config.author)
