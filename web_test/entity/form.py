15ad7ec89db3a6fb100abc675cbdf1d85881fc0f
xgu <g_xin@outlook.com>
2023-03-27 12:52:01 +0800

    Fix

--- web_test/entity/form.py
+++ web_test/entity/form.py
@@ -16,14 +16,9 @@ class FormEntity(ABCForm):
         self.form_data_by_index = FormData(self.header_locator, self.row_locator_by_index)
         self.form_data_by_keyword = FormData(self.header_locator, self.row_locator_by_keyword)
 
-    def row_keyword_should_be_visible(self, row_keyword):
-        logger.info(f'检查表单信息{row_keyword}是否可见')
-        s(by.text(row_keyword)).should(be.visible)
-        return self
-
-    def row_keyword_should_not_be_visible(self, row_keyword):
-        logger.info(f'检查表单信息{row_keyword}是否不可见')
-        s(by.text(row_keyword)).should(be.not_.visible)
+    def check_row_keyword_visible(self, row_keyword):
+        visible = s(by.text(row_keyword)).wait_until(be.visible)
+        logger.info(f'检查表单信息{row_keyword}是否可见：{visible}')
         return self
 
     def click_row_keyword(self, row_keyword):
