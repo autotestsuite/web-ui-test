cf2aada95ffb75f97b228ea93794c5067c0119aa
xgu <g_xin@outlook.com>
2023-03-28 14:22:31 +0800

    Fix

--- web_test/model/locator.py
+++ web_test/model/locator.py
@@ -7,20 +7,7 @@ from selene.support.shared.jquery_style import ss
 
 
 class LocatorConfig:
-    locate_rule: Callable[[str], str] = None
-
-    input_box: Label = (lambda: Label(LocatorConfig.locate_rule, 'input'))
-    textarea: Label = (lambda: Label(LocatorConfig.locate_rule, 'textarea'))
-    text: Label = (lambda: Label(LocatorConfig.locate_rule, 'span'))
-    select: Label = (lambda: Label(LocatorConfig.locate_rule, 'select'))
-    button: Label = (lambda: Label(LocatorConfig.locate_rule, 'button'))
-
-
-class Label:
-
-    def __init__(self, locate_function: Callable[[str], str], tag: str):
-        self.locate_function = locate_function
-        self.tag = tag
+    locate_function: Callable[[str], str] = None
 
     def behind_label(self, label_value):
         by_, value = by.text(label_value)
@@ -31,3 +18,7 @@ class Label:
         by_, value = by.text(label_value)
         xpath = f'{self.locate_function(value)}//{self.tag}'
         return ss(by.xpath(xpath))
+
+    def __getattr__(self, tag: str):
+        self.tag = tag
+        return self
